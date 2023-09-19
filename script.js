bea97224571d9ea59e5de49dbfd13b19496adc56
uShire <afgregana@up.edu.ph>
2023-09-19 21:54:24 +0800

    091823 lecture

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ const burgerBtn = document.querySelector(".burger__btn");
 const headerNav = document.querySelector(".header__nav");
 burgerBtn.addEventListener("click", () => {
   headerNav.classList.toggle("open");
+  burgerBtn.classList.toggle("open");
 });
